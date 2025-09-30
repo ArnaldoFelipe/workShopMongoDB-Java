fc9741b63a73417550a2a7532076d26a12de249c
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-09-30 00:42:02 -0300

    iniciando meu novo projeto

---
+++ src/main/java/com/project/workShopMongoDB/WorkShopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.project.workShopMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/workShopMongoDB/WorkShopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.workShopMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
