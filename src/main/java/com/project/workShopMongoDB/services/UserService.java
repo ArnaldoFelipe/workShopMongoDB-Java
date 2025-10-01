8ec11b8eb1f457e0f508dc9688a2ee58a585579a
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-10-01 00:26:59 -0300

    criando o metodo para deletar usuario por id

--- src/main/java/com/project/workShopMongoDB/services/UserService.java
+++ src/main/java/com/project/workShopMongoDB/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.workShopMongoDB.domain.User;
+import com.project.workShopMongoDB.dto.UserDto;
 import com.project.workShopMongoDB.exception.ObjectNotFoundException;
 import com.project.workShopMongoDB.repository.UserRepository;
 
@@ -26,4 +27,20 @@ public class UserService {
         
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado, id nao existe"));
     }
+
+    public User insert(User user){
+        return repository.insert(user);
+    }
+
+    public void delete(String id){
+        findById(id);
+        repository.deleteById(id);
+    }
+
+    public User fromDto(UserDto userDto){
+        return new User(userDto.getId(),userDto.getName(), userDto.getEmail());
+        
+    }
+
+    
 }
