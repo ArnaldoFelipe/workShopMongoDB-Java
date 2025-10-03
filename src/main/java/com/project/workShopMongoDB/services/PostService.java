918eae385a3a429ccf8be21bf1906035ecd3387b
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-10-03 01:19:08 -0300

    ajustando erros de escrita

--- src/main/java/com/project/workShopMongoDB/services/PostService.java
+++ src/main/java/com/project/workShopMongoDB/services/PostService.java
@@ -4,15 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.project.workShopMongoDB.domain.Post;
 import com.project.workShopMongoDB.exception.ObjectNotFoundException;
 import com.project.workShopMongoDB.repository.PostRepository;
 
+@Service
 public class PostService {
     
     @Autowired
-    PostRepository postRepository;
+    private PostRepository postRepository;
 
     public Post findById(String id){
         
