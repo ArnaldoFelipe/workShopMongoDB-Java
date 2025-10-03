2e9373aaf37f0c666327b9c8c17f6677b3666354
arnaldo felipe <feelipesilvaa11@gmail.com>
2025-10-03 00:16:01 -0300

    consulta simples com query methods

--- src/main/java/com/project/workShopMongoDB/services/PostService.java
+++ src/main/java/com/project/workShopMongoDB/services/PostService.java
@@ -1,5 +1,6 @@
 package com.project.workShopMongoDB.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +18,9 @@ public class PostService {
         
         Optional<Post> post = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado, id nao existe"));
+    }
+
+    public List<Post> findByTitle(String text){
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
